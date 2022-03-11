94db62e1a2ead39c2b2d68ae7115fc44e2b196cf
Lee Joo Hyung <wngud985@gmail.com>
2022-03-11 23:53:27 +0900

    vue cli + form practice

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+export default {
+  data: function () {
+    return {
+      str: "hello",
+    };
+  },
+  components: {
+    "app-header": AppHeader,
+  },
+  methods: {
+    renewStr: function () {
+      this.str = "changed";
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ vue-form/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id:</label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">pw:</label>
+      <input id="password" type="password" v-model="password" />
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      username: "",
+      password: "",
+      message: "",
+    };
+  },
+  methods: {
+    submitForm: async function () {
+      console.log(this.username, this.password);
+      var url = "https://jsonplaceholder.typicode.com/users";
+      var data = {
+        username: this.username,
+        password: this.password,
+      };
+      try {
+        let res = await axios.post(url, data);
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
